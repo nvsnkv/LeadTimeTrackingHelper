d760340aea8ea9e9d35c839dea628cd918baf804
Nikita V Senkov <nvsnkv@live.com>
2015-10-04 14:11:10 +0300

    simple validation

--- src/TeamUtils.LeadTimeTrackingHelper/Models/TrackChangeRequest.cs
+++ src/TeamUtils.LeadTimeTrackingHelper/Models/TrackChangeRequest.cs
@@ -1,15 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using TeamUtils.LeadTimeTrackingHelper.Models.Validation;
 
 namespace TeamUtils.LeadTimeTrackingHelper.Models
 {
     public class TrackChangeRequest
     {
+        [Required]
         public string Activity { get; set; }
+
+        [Required]
         public string From { get; set; }
+
+        [Required, NotMatch("From")]
         public string To { get; set; }
+
+        [Timestamp]
         public string Timestamp { get; set; }
     }
 }
---
+++ src/TeamUtils.LeadTimeTrackingHelper/Models/Validation/NotMatchAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeamUtils.LeadTimeTrackingHelper.Models.Validation
+{
+    public class NotMatchAttribute:CompareAttribute
+    {
+        private readonly string _otherProperty;
+
+        public NotMatchAttribute(string otherProperty):base(otherProperty)
+        {
+            _otherProperty = otherProperty;
+            ErrorMessage = "{0} and {1} should not be equal";
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var result = base.IsValid(value, validationContext);
+            if (result != ValidationResult.Success)
+                return ValidationResult.Success;
+
+            return new ValidationResult(FormatErrorMessage(_otherProperty));
+        }
+    }
+}
